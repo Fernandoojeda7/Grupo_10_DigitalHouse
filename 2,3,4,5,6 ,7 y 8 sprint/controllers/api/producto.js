6794a1f9f7659d6ba2f476832e973fc4875ffe08
Fernando Ojeda <fernandoojeda7@gmail.com>
2022-04-28 21:13:00 -0300

    api productos terminado

--- 2,3,4,5,6 ,7 y 8 sprint/controllers/api/producto.js
+++ 2,3,4,5,6 ,7 y 8 sprint/controllers/api/producto.js
@@ -1,34 +1,65 @@
 const db = require('../../database/models');
+const Sequelize = require('sequelize')
 const Op = db.sequelize;
 
 const Productos = db.Producto;
+const Proveedor = db.Proveedor;
 
-const controller= {
+const controller = {
 
-    lista: (req, res)=> {
-        Productos.findAll()
-        .then(productos => {
-            let response = {
-                status:200,
-                total: productos.length,
-                
-            }
-            return res.status(200).json({response})
-        })
+    lista: async (req, res) => {
+        let individualesCount = await Productos.count(
+            {
+                attributes: ['category'],
+                group: 'category',
 
+            })
+        let generalCount = await Productos.count();
+
+        let totalProductos = await Productos.findAll();
+
+        let productosAProveedor = await Productos.count({
+            include: [{model: Proveedor, as: 'proveedores'}],
+            distinct: 'id' 
+        });
+
+        let allProducts = await totalProductos.map(producto => ({
+            id: producto.id,
+            nombre: producto.name,
+            descripcion:producto.description,
+            detail: req.originalUrl + '/' + producto.id
+            }))
+
+        return res.json({
+            total: generalCount,
+            individuals: individualesCount,
+            array: productosAProveedor,
+            products: allProducts
+            })
     },
-        detalle: (req, res) => {
-            Productos.findByPk(req.params.id)
-            .then((producto)=>{
+    
+    detalle: (req, res) => {
+        Productos.findByPk(req.params.id)
+            .then((producto) => {
                 return res.status(200).json({
                     status: 200,
-                    Producto: producto
+                    Producto: [{
+                        Id: producto.id,
+                        nombre: producto.name,
+                        precio: producto.price,
+                        descuento: producto.descount,
+                        tipo: producto.type,
+                        categoria: producto.category,
+                        descripcion: producto.description,
+                        imagen: 'http://localhost:3030/img/products' + '/' + producto.image
+    
+                    }]
+                    })
                 })
-            })
-            
 
-        }
-        
+
+    }
+
 
 
 }
